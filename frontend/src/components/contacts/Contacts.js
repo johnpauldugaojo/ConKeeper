f56e444fe570a7ac239a7d259eaf80154edb037f
unknown <johnpauldugaojo@gmail.com>
2021-06-14 18:56:52 +0800

    Adding Authentication State

--- frontend/src/components/contacts/Contacts.js
+++ frontend/src/components/contacts/Contacts.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext } from 'react';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/ContactContext';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Contacts = () => {
   // Init Context
@@ -14,15 +15,22 @@ const Contacts = () => {
 
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map((contact) => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))
-        : contacts.map((contact) => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
 
 export default Contacts;
+// Nilipat ung key sa CSS Transition
